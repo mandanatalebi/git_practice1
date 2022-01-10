f6b6fea44698e7d379dbeed02a626feb96ce27dd
Mandana <tale1509@pacificu.edu>
2022-01-10 11:45:09 -0800

    Work 3 is done and about to be pushed to Guithub after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld {
 
     //Doing some work here(You can type anything you want)
 
-//Doing awesome work 2!
+    //Doing awesome work 2!
 
+    //Doing awesome work 3!
+
+    //Awesome work 3 is done and ready to be committed
+    //and readt to be pushed to remote repository
 }
